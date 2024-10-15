528bc8016fdd3feadc26acc05fbaee7c20b57403
Michael Joshua Ramos <michael.joshua.millevo.ramos@gmail.com>
2024-10-15 18:38:47 +0800

    Set up basic routing with react-router-dom using createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+
+    children: [
+      {
+        index: true,
+        element: <div>Landing</div>,
+      },
+      {
+        path: "menu",
+        element: <div>Menu</div>,
+      },
+      {
+        path: "menu/:name",
+        element: <div>Menu Item</div>,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
