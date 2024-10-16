184a102da2f56094878db87032599a9947b9cb83
Michael Joshua Ramos <michael.joshua.millevo.ramos@gmail.com>
2024-10-16 23:02:06 +0800

    Add SweetgreenWorkSection component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
         "sweetgreen-black": "rgb(14, 21, 14)",
         "sweetgreen-green-primary": "rgb(0, 71, 60)",
         "sweetgreen-green-secondary": "rgb(169, 193, 169)",
+        "sweetgreen-green-tertiary": "rgb(216, 229, 214)",
+        "sweetgreen-green-sand": "rgb(232, 220, 198)",
       },
     },
   },
