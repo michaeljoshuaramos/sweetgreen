8dd130fb14e04f4547bd0adf1465e78e9ce4dee8
Michael Joshua Ramos <michael.joshua.millevo.ramos@gmail.com>
2024-10-17 14:22:09 +0800

    Add NewsSection and NewsCard components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
+export { default as Footer } from "./Footer";
 export { default as MenuItemCard } from "./MenuItemCard";
 export { default as NavigationBar } from "./NavigationBar";
+export { default as NewsCard } from "./NewsCard";
+export { default as NewsletterSection } from "./NewsletterSection";
 export { default as SweetgreenWorkSection } from "./SweetgreenWorkSection";
 export { default as SweetpassSection } from "./SweetpassSection";
