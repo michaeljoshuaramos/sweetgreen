a3bcd7b1a4c090d3f42d06c4f76d3d2fdfc13345
Michael Joshua Ramos <michael.joshua.millevo.ramos@gmail.com>
2024-10-17 16:38:05 +0800

    Add hover style for NewsCard component

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -2,7 +2,7 @@ import { type News } from "../utils";
 
 const NewsCard = ({ news }: { news: News }) => {
   return (
-    <div className="bg-sweetgreen-sand rounded-3xl p-4 flex">
+    <div className="bg-sweetgreen-sand rounded-3xl p-4 flex group hover:cursor-pointer">
       <article className="text-sm">
         <div className="flex flex-col h-full justify-between">
           <div>
@@ -13,12 +13,20 @@ const NewsCard = ({ news }: { news: News }) => {
             <p>
               {news.teaser}
               <br />
-              <span>Read more →</span>
+              <span className="group-hover:border-b group-hover:border-sweetgreen-green-primary">
+                Read more →
+              </span>
             </p>
           </div>
         </div>
       </article>
-      <img src={news.image} alt={news.title} className="rounded-2xl max-w-44" />
+      <div className="max-w-44 overflow-hidden rounded-2xl">
+        <img
+          src={news.image}
+          alt={news.title}
+          className="object-cover w-full h-auto transform transition duration-300 group-hover:scale-110 group-hover:brightness-90"
+        />
+      </div>
     </div>
   );
 };
