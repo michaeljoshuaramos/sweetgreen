7a1f2e9a3e28edbc35c98764db729c170b8b6c34
Michael Joshua Ramos <michael.joshua.millevo.ramos@gmail.com>
2024-10-18 13:20:27 +0800

    Setup shadcn/ui in my Vite project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,64 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "sweetgreen-yellow": "rgb(244, 243, 231)",
-        "sweetgreen-black": "rgb(14, 21, 14)",
-        "sweetgreen-green-primary": "rgb(0, 71, 60)",
-        "sweetgreen-green-secondary": "rgb(169, 193, 169)",
-        "sweetgreen-green-tertiary": "rgb(216, 229, 214)",
-        "sweetgreen-sand": "rgb(232, 220, 198)",
-        "sweetgreen-neon": "rgb(230, 255, 85)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			'sweetgreen-yellow': 'rgb(244, 243, 231)',
+  			'sweetgreen-black': 'rgb(14, 21, 14)',
+  			'sweetgreen-green-primary': 'rgb(0, 71, 60)',
+  			'sweetgreen-green-secondary': 'rgb(169, 193, 169)',
+  			'sweetgreen-green-tertiary': 'rgb(216, 229, 214)',
+  			'sweetgreen-sand': 'rgb(232, 220, 198)',
+  			'sweetgreen-neon': 'rgb(230, 255, 85)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
